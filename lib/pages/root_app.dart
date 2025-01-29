2ed0a55260a8be523a91adc14aed6b2c6e7bfee6
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-01-30 01:06:41 +0900

    rootapp

--- lib/pages/root_app.dart
+++ lib/pages/root_app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_telegrame_clone/theme/colors.dart';
 
 class RootApp extends StatefulWidget {
   const RootApp({Key? key}) : super(key: key);
@@ -10,6 +11,43 @@ class RootApp extends StatefulWidget {
 class _RootAppState extends State<RootApp> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Hello World');
+    return Scaffold(
+      backgroundColor: bgColor,
+      bottomNavigationBar: getFooter(),
+    );
+  }
+
+  Widget getFooter() {
+    List iconsItems = [
+      Icons.account_circle,
+      Icons.chat,
+      Icons.settings,
+    ];
+    List textItems = [
+      "Contacts",
+      "Chats",
+      "Settings",
+    ];
+    return Container(
+        height: 90,
+        width: double.infinity,
+        decoration: BoxDecoration(color: greyColor),
+        child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: List.generate(3, (index) {
+                return Column(
+                  children: [
+                    Icon(iconsItems[index],
+                        size: 30, color: white.withOpacity(0.5)),
+                    SizedBox(height: 3),
+                    Text(textItems[index],
+                        style: TextStyle(
+                            fontSize: 11, color: white.withOpacity(0.5)))
+                  ],
+                );
+              }),
+            )));
   }
 }
