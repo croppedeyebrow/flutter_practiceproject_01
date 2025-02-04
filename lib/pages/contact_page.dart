4d1eb8ab190f37cfeb26bc2d7305326adf7431d4
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-02-04 18:07:03 +0900

    Appbar

--- lib/pages/contact_page.dart
+++ lib/pages/contact_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_telegrame_clone/theme/colors.dart';
 
 class ContactPage extends StatefulWidget {
   const ContactPage({Key? key}) : super(key: key);
@@ -10,6 +11,27 @@ class ContactPage extends StatefulWidget {
 class _ContactPageState extends State<ContactPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: bgColor,
+      appBar: PreferredSize(
+        child: getAppBar(),
+        preferredSize: Size.fromHeight(80),
+      ),
+    );
+  }
+
+  Widget getAppBar() {
+    return AppBar(
+      elevation: 0,
+      backgroundColor: greyColor,
+      title: Text("Contacts"),
+      leading: IconButton(
+          onPressed: null,
+          icon: Text(
+            "Sort",
+            style: TextStyle(
+                fontSize: 16, color: primary, fontWeight: FontWeight.w500),
+          )),
+    );
   }
 }
