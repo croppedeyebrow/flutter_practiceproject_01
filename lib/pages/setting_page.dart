1fc76da33f34130b552ebe6215ed421f83a555ba
Croppedeyebrow <142734435+croppedeyebrow@users.noreply.github.com>
2025-03-23 02:08:48 +0900

    finish

--- lib/pages/setting_page.dart
+++ lib/pages/setting_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_telegrame_clone/json/chat_json.dart';
+import 'package:flutter_telegrame_clone/json/setting_json.dart';
 import 'package:flutter_telegrame_clone/theme/colors.dart';
 
 class SettingPage extends StatefulWidget {
@@ -43,34 +44,194 @@ class _SettingPageState extends State<SettingPage> {
   }
 
   Widget getBody() {
-    return Column(children: [
-      Center(
-        child: Container(
-          width: 90,
-          height: 90,
-          decoration: BoxDecoration(
-            shape: BoxShape.circle,
-            image: DecorationImage(
-                image: NetworkImage(profile[0]['img']), fit: BoxFit.cover),
+    return SingleChildScrollView(
+      child: Column(children: [
+        Center(
+          child: Container(
+            width: 90,
+            height: 90,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              image: DecorationImage(
+                  image: NetworkImage(profile[0]['img']), fit: BoxFit.cover),
+            ),
           ),
         ),
-      ),
-      SizedBox(height: 20),
-      Text(
-        profile[0]['name'],
-        style:
-            TextStyle(fontSize: 24, color: white, fontWeight: FontWeight.w600),
-      ),
-      SizedBox(
-        height: 2,
-      ),
-      Text(
-        "+855 10 101 101 - @sopheamen",
-        style: TextStyle(
-            fontSize: 18,
+        SizedBox(height: 20),
+        Text(
+          profile[0]['name'],
+          style: TextStyle(
+              fontSize: 24, color: white, fontWeight: FontWeight.w600),
+        ),
+        SizedBox(
+          height: 2,
+        ),
+        Text(
+          "+855 10 101 101 - @sopheamen",
+          style: TextStyle(
+              fontSize: 18,
+              color: white.withAlpha(50),
+              fontWeight: FontWeight.w500),
+        ),
+        SizedBox(height: 30),
+        getSectionOne(),
+        SizedBox(height: 30),
+        getSectionTwo(),
+        SizedBox(height: 30),
+        getSectionThree(),
+        SizedBox(height: 30),
+      ]),
+    );
+  }
+
+  Widget getSectionOne() {
+    return Column(
+        children: List.generate(setting_section_one.length, (index) {
+      return Container(
+          width: double.infinity,
+          decoration: BoxDecoration(color: textfieldColor),
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, bottom: 8, top: 8),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              color: setting_section_one[index]['color'],
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Center(
+                            child: Icon(setting_section_one[index]['icon'],
+                                color: white, size: 20),
+                          )),
+                      SizedBox(width: 12),
+                      Text(setting_section_one[index]['text'],
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: white,
+                              fontWeight: FontWeight.w500))
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios,
+                      color: white.withAlpha(20), size: 14)
+                ],
+              )
+            ]),
+          ));
+    }));
+  }
+
+  Widget getSectionTwo() {
+    return Column(
+        children: List.generate(setting_section_two.length, (index) {
+      return Container(
+          width: double.infinity,
+          decoration: BoxDecoration(color: textfieldColor),
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, bottom: 8, top: 8),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              color: setting_section_two[index]['color'],
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Center(
+                            child: Icon(setting_section_two[index]['icon'],
+                                color: white, size: 20),
+                          )),
+                      SizedBox(width: 12),
+                      Text(setting_section_two[index]['text'],
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: white,
+                              fontWeight: FontWeight.w500))
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      getLangAndSticker(setting_section_two[index]['text']),
+                      SizedBox(width: 5),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        color: white.withAlpha(20),
+                        size: 14,
+                      )
+                    ],
+                  )
+                ],
+              )
+            ]),
+          ));
+    }));
+  }
+
+  getLangAndSticker(value) {
+    if (value == 'Language') {
+      return Text("English",
+          style: TextStyle(
+            fontSize: 14,
             color: white.withAlpha(50),
-            fontWeight: FontWeight.w500),
-      )
-    ]);
+          ));
+    } else if (value == 'Stickers and Emojis') {
+      return Badge(
+          backgroundColor: primary,
+          label: Text("12",
+              style: TextStyle(fontSize: 15, color: white.withAlpha(50))));
+    }
+    return Container();
+  }
+
+  Widget getSectionThree() {
+    return Column(
+        children: List.generate(setting_section_three.length, (index) {
+      return Container(
+          width: double.infinity,
+          decoration: BoxDecoration(color: textfieldColor),
+          child: Padding(
+            padding:
+                const EdgeInsets.only(left: 10, right: 10, bottom: 8, top: 8),
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                          width: 30,
+                          height: 30,
+                          decoration: BoxDecoration(
+                              color: setting_section_three[index]['color'],
+                              borderRadius: BorderRadius.circular(8)),
+                          child: Center(
+                            child: Icon(setting_section_three[index]['icon'],
+                                color: white, size: 20),
+                          )),
+                      SizedBox(width: 12),
+                      Text(setting_section_three[index]['text'],
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: white,
+                              fontWeight: FontWeight.w500))
+                    ],
+                  ),
+                  Icon(Icons.arrow_forward_ios,
+                      color: white.withAlpha(20), size: 14)
+                ],
+              )
+            ]),
+          ));
+    }));
   }
 }
